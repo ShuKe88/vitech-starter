e10ec33c0ea814c4e3c7c3636a3661064e6e80ee
ShuKe88 <978874652@qq.com>
2024-11-17 17:06:15 +0800

    使用集成tailwindcss的unocss、使用vite-plugin-vue-layouts、使用nplugin-vue-router

--- eslint.config.js
+++ eslint.config.js
@@ -17,18 +17,22 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
       'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
+      'cypress/support/**/*.{js,ts,jsx,tsx}',
     ],
+  },
+  {
+    name: 'vue/multi-word-component-names',
+    ignores: [],
   },
   skipFormatting,
 ]
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueRouter from 'unplugin-vue-router/vite'
+import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      /* options */
+    }),
     vue(),
     vueJsx(),
+    UnoCSS(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
