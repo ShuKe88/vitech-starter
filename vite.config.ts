491b52c9b6f0b1b966841b921bbf74879fe9bec0
ShuKe88 <978874652@qq.com>
2024-11-19 22:37:14 +0800

    feat: add unplugin-auto-import自动导入第三方库的API 和 unplugin-vue-components自动导入组件

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,30 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueRouter from 'unplugin-vue-router/vite'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Components from 'unplugin-vue-components/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    VueRouter({
-      /* options */
-    }),
+    VueRouter({}),
     vue(),
     vueJsx(),
     UnoCSS(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+
+      // global imports to register
+      imports: ['vue', VueRouterAutoImports, '@vueuse/core'],
+    }),
+    Components({
+      directoryAsNamespace: true, // 深层级组件在使用时需要将子文件名作为前缀
+      collapseSamePrefixes: true, // 当深层组件的前缀和子文件的名称相同时,省略前缀
+    }),
   ],
 })
